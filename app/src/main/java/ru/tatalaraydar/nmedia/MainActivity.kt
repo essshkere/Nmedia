aeae04484bf1852d91412e5ff77fb3285c8ef306
Aydar <hispooy@gmail.com>
2024-11-09 01:09:14 +0500

    button like, not complete 2

--- app/src/main/java/ru/tatalaraydar/nmedia/MainActivity.kt
+++ app/src/main/java/ru/tatalaraydar/nmedia/MainActivity.kt
@@ -8,22 +8,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ru.tatalaraydar.nmedia.databinding.ActivityMainBinding
+import ru.tatalaraydar.nmedia.dto.Post
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(binding.root)
-        findViewById<ImageView>(R.id.button_likes).setOnClickListener {
-        println("rddf")
-
+        val post = Post(
+            1, "Нетология. Университет интернет-профессий будущего", "net", "10 мая в 19:45",
+            "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            100000, 5, 10, false  )
+        binding.content.text = post.content
+        binding.published.text = post.published
+        binding.author.text = post.author
+        updatelike(binding,post)
+        binding.buttonLikes.setOnClickListener {
+            post.likedByMe = !post.likedByMe
+            updatelike(binding,post)
         }
-    //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.menu)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+    }
+
+    private fun updatelike(binding: ActivityMainBinding, post: Post) {
+        binding.buttonLikes.setImageResource(
+            if (post.likedByMe) {
+                post.likes += 1
+                R.drawable.baseline_thumb_up_red
+            } else {
+                post.likes -= 1
+                R.drawable.baseline_thumb_up_alt_24
+            }
+        )
     }
 }
