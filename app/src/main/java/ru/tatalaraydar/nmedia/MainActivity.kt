0308e2632f2d8ec0f390d52c5248d369ef794a4d
Aydar <hispooy@gmail.com>
2024-11-11 21:26:38 +0500

    correction fun formatCount

--- app/src/main/java/ru/tatalaraydar/nmedia/MainActivity.kt
+++ app/src/main/java/ru/tatalaraydar/nmedia/MainActivity.kt
@@ -55,9 +55,8 @@ class MainActivity : AppCompatActivity() {
 
     fun formatCount(count: Int): String {
         return when {
-            count >= 1_000_000 -> String.format("%.1fM", count / 1_000_000.0)
-                .replace(",", ".")
-
+            count >= 1_000_000 -> String.format("%.1fM", count / 1_000_000.0).replace(",", ".")
+            count >= 1_100 -> String.format("%.1fK", count / 1_000.0).replace(",", ".")
             count >= 1_000 -> "${count / 1_000}K"
             else -> count.toString()
         }
