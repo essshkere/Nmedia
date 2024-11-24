a9f25cabe89808147fa89995a3138a9ca9f46a3e
Aydar <hispooy@gmail.com>
2024-11-24 19:10:22 +0500

    task 1 complete

--- app/src/main/java/ru/tatalaraydar/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/tatalaraydar/nmedia/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.tatalaraydar.nmedia.activity
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
@@ -16,12 +17,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val viewModel: PostViewModel by viewModels()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val newPostLauncher = registerForActivityResult(NewPostActivity.NewPostContract){ result ->
+        val newPostLauncher = registerForActivityResult(NewPostActivity.NewPostContract) { result ->
             result ?: return@registerForActivityResult
             viewModel.сhangeContent(result)
             viewModel.save()
@@ -37,6 +39,14 @@ class MainActivity : AppCompatActivity() {
                 viewModel.like(post.id)
             }
 
+            override fun onEdit(post: Post) {
+                val intent = Intent(this@MainActivity, EditPostActivity::class.java).apply {
+                    putExtra("post_id", post.id)
+                    putExtra("post_content", post.content)
+                }
+                startActivityForResult(intent, EDIT_POST_REQUEST_CODE)
+            }
+
             override fun onShare(post: Post) {
                 val intent = Intent().apply {
                     action = Intent.ACTION_SEND
@@ -46,7 +56,8 @@ class MainActivity : AppCompatActivity() {
                 val shareIntent =
                     Intent.createChooser(intent, getString(R.string.chooser_share_post))
                 startActivity(shareIntent)
-            }})
+            }
+        })
 
         binding.container.adapter = adapter
 
@@ -57,6 +68,18 @@ class MainActivity : AppCompatActivity() {
         binding.save.setOnClickListener {
             newPostLauncher.launch(Unit)
         }
-
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == EDIT_POST_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val updatedContent = data?.getStringExtra("updated_content") ?: return
+            val postId = data.getLongExtra("post_id", 0L)
+            viewModel.сhangeContent(updatedContent)
+            viewModel.save()
+        }
+    }
+    companion object {
+        private const val EDIT_POST_REQUEST_CODE = 100 // Определите код запроса
     }
 }
+
