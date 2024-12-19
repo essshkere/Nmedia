e05becd202108a1a81b3071a1f6b4d85944c2042
Aydar work <work@work>
2024-12-19 12:36:52 +0500

    notification

--- app/src/main/java/ru/tatalaraydar/nmedia/service/FCMService.kt
+++ app/src/main/java/ru/tatalaraydar/nmedia/service/FCMService.kt
@@ -1,21 +1,143 @@
 package ru.tatalaraydar.nmedia.service
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.google.gson.Gson
+
+import ru.tatalaraydar.nmedia.R
+import kotlin.random.Random
 
 class FCMService : FirebaseMessagingService() {
+    private val action = "action"
+    private val content = "content"
+    private val channelId = "remote"
+    private val gson = Gson()
+
+
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_remote_name)
+            val descriptionText = getString(R.string.channel_remote_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+
 
     override fun onNewToken(token: String) {
         Log.d("FCM", token)
         println(token)
     }
     override fun onMessageReceived(message: RemoteMessage) {
-
-//        message.data[action]?.let {
-//            when (Action.valueOf(it)) {
-//                Action.LIKE -> handleLike(gson.fromJson(message.data[content], Like::class.java))
-//            }
-//        }
+        Log.d("FCM2", "message receive")
+        message.data[action]?.let {
+            try {
+                when (Action.valueOf(it)) {
+                    Action.LIKE -> handleLike(gson.fromJson(message.data[content], Like::class.java))
+                    Action.POST -> handlePost(gson.fromJson(message.data[content], Post::class.java))
+                    else -> handleDefaultAction()
+                }
+            } catch (e: IllegalArgumentException) {
+                Log.e("FCM2", "other message")
+                handleDefaultAction()
+            }
+        }
     }
+
+    private fun handleDefaultAction() {
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(
+                getString(R.string.notification_other)
+            )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        notify(notification)
+
+        Log.d("FCM2", "Неизвестное действие получено")
+    }
+
+    private fun handleLike(content: Like) {
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(
+                getString(
+                    R.string.notification_user_liked,
+                    content.userName,
+                    content.postAuthor,
+                )
+            )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        notify(notification)
+    }
+
+    private fun handlePost(content: Post) {
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(
+                getString(
+                    R.string.notification_post,
+                    content.userName
+                )
+            )
+            .setStyle(NotificationCompat.BigTextStyle().bigText(content.contentPost))
+
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        notify(notification)
+    }
+
+
+    private fun notify(notification: Notification) {
+        if (
+            Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU ||
+            checkSelfPermission(
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            NotificationManagerCompat.from(this)
+                .notify(Random.nextInt(100_000), notification)
+        }
+    }
+
+    enum class Action {
+        LIKE,
+        POST,
+    }
+
+    data class Like(
+        val userId: Long,
+        val userName: String,
+        val postId: Long,
+        val postAuthor: String,
+    )
+
+    data class Post(
+        val userId: Long,
+        val userName: String,
+        val postId: Long,
+        val postAuthor: String,
+        val contentPost: String
+    )
+
 }
